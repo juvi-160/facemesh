a3d5c9a5d9a0f27289f3172b5f27b55a6941945f
juvi-160 <juveriayameen160@gmail.com>
2026-09-23 17:52:29 +0530

    Add initial project files and setup for React application
    - Added logo image (logo512.png) for the application.
    - Created manifest.json for PWA configuration.
    - Added robots.txt to manage web crawler access.
    - Implemented basic CSS styles in App.css.
    - Developed main application component in App.js.
    - Created a test for the App component in App.test.js.
    - Established global styles in index.css.
    - Set up entry point in index.js for React application.
    - Included SVG logo file (logo.svg) for branding.
    - Implemented performance reporting in reportWebVitals.js.
    - Configured testing environment with setupTests.js.

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// Install Dependencies DONE
+// import dependencies
+// set up webcam and canvas
+// define refrences to those 
+// load facemesh 
+// detect function
+// drawing utilities
+// load triangulation
+// setup triangle path 
+// setup point drawing
+// add drawMesh to detect function
+
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
